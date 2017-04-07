7dfda51a9802be1eaed195d1bedf63e6980c97fc
Dakota Hawkins <dakotahawkins0110@gmail.com>
2017-04-06 21:49:15 -0400

    Added 'add' and 'iadd' overload. Fixed 'str' overload. Changed num_syllables to n_syllables.

--- SyllableCollection.py
+++ SyllableCollection.py
@@ -5,11 +5,29 @@ class SyllableCollection(object):
     def __init__(self, syllables = []):
         self.__check_inputs(syllables)
         self.syllables = syllables
-        self.num_syllables = len(syllables)
+        self.n_syllables = len(syllables)
 
     def __str__(self):
+        out_str = ''
         for each in self.syllables:
-            print(each)
+            out_str += str(each)
+        return(out_str)
+
+    def __add__(self, other):
+        if isinstance(other, SyllableCollection):
+            return(SyllableCollection(self.syllables + other.syllables))
+        else:
+            raise TypeError('Cannot concatenate \
+            SyllableCollection with %s' % type(other))
+
+    def __iadd__(self, other):
+        if isinstance(other, SyllableCollection):
+            self.syllables = self.syllables + other.syllables
+            self.n_syllables = len(self.syllables)
+            return(self)
+        else:
+            raise TypeError('Cannot concatenate \
+            SyllableCollection with %s' % type(other))
 
     def __getitem__(self, i):
         if isinstance(i, slice):
@@ -25,14 +43,14 @@ class SyllableCollection(object):
             return(self.syllables[i])
 
         else:
-            raise TypeError('Invalid argument type.')
+            raise TypeError('Invalid argument type: %s.' % type(i))
 
     def __len__(self):
-        return self.num_syllables
+        return(self.n_syllables)
 
     def __check_inputs(self, syllables):
-        if not isinstance(syllables, list) and not isinstance(syllables, Syllable):
-            raise TypeError('<syllables> must be a list of syllables or single syllable instance.')
+        if not isinstance(syllables, list):
+            raise TypeError('<syllables> must be a list of syllables.')
         if len(syllables) > 0 and not any([self.__check_syllable(each) for each in syllables]):
             raise TypeError('Non-syllable class contained in input list.')
 
@@ -43,13 +61,17 @@ class SyllableCollection(object):
 
     def add_syllable(self, new_syllable):
         if not self.__check_syllable(new_syllable):
-            raise TypeError('Attempting to set non-Syllable entry.')
+            raise TypeError('Attempting to set non-Syllable entry: %s' %
+                             type(new_syllable))
         self.syllables.append(new_syllable)
-        self.num_syllables += 1
+        self.n_syllables += 1
 
     def get_labels(self):
         label_list = [each.label for each in self.syllables]
         return(label_list)
+
+    def get_unique_syllables(self):
+        return(list(set(self.get_labels())))
 
     def get_start_times(self):
         start_list = [each.start for each in self.syllables]
@@ -59,14 +81,9 @@ class SyllableCollection(object):
         end_list = [each.start for each in self.syllables]
         return(end_list)
 
-    def get_unique_syllables(self):
-        return(list(set(self.get_labels())))
 
 if __name__ == '__main__':
-    from Recording import Recording
-    recording_test = Recording('Downloads/CATH1.wav',
-                               'Downloads/CATH1.TextGrid', 'CATH1')
-    print(recording_test)
-    recording_test.get_annotations(keep_background=False, keep_songs=False)
-    print(recording_test)
-    print(len(recording_test.syllables.get_unique_syllables()))
+    syl_1 = Syllable(start=12.19, end=12.43, sound_file='TestData/CATH1.wav')
+    syl_2 = Syllable(start=12.60, end=12.85, sound_file='TestData/CATH1.wav')
+    syl_3 = Syllable(start=13.03, end=13.19, sound_file='TestData/CATH1.wav')
+    syl_col_test = SyllableCollection([syl_1, syl_2, syl_3])
