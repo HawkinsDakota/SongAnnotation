92556a5a90b6fffcc009d9127c6063f638e18ef0
Dakota Hawkins <dakotahawkins0110@gmail.com>
2017-04-19 19:55:37 -0400

    Changes to allow weighted run.

--- run_lstm_model.py
+++ run_lstm_model.py
@@ -5,6 +5,8 @@ from SyllableLSTM import SyllableLSTM
 import SyllablePlots
 import pickle
 import pandas
+import numpy
+import torch
 from sklearn.model_selection import train_test_split
 
 def load_dataset(read_file):
@@ -21,27 +23,32 @@ def create_training_and_test(syllables, fold=10):
                                       stratify=syllables.get_labels())
     return(training, test)
 
+def calculate_weights(syllables):
+    unique, counts = numpy.unique(syllables, return_counts=True)
+    weights = counts/sum(counts)
+    return(list(unique), torch.from_numpy(weights).float())
 
 dataset = load_dataset('all_songs.pkl')
-SyllablePlots.plot_syllable_distribution(dataset.syllable_labels,
-                                         save_file='all_songs_dist.csv',
-                                         plot_file='all_songs_dist.png')
+# SyllablePlots.plot_syllable_distribution(dataset.syllable_labels,
+#                                          save_file='all_songs_dist.csv',
+#                                          plot_file='all_songs_dist.png')
 count_df = pandas.read_csv('all_songs_dist.csv')
 keep_labels = count_df['Syllable'].loc[count_df['Count'] > 5].values
 
 keep_syllables = SyllableCollection([each for each in dataset.syllables if each.label in keep_labels])
 
-SyllablePlots.plot_syllable_distribution(keep_syllables.get_labels(),
-                                         save_file='subset_syl_dist.csv',
-                                         plot_file='subset_syl_dist.png')
+# SyllablePlots.plot_syllable_distribution(keep_syllables.get_labels(),
+#                                          save_file='subset_syl_dist.csv',
+#                                          plot_file='subset_syl_dist.png')
 
-syllable_lstm = SyllableLSTM(128, keep_syllables.get_unique_syllables())
 training, test = create_training_and_test(keep_syllables)
+unique_syllables, training_weights = calculate_weights(keep_syllables.get_labels())
+syllable_lstm = SyllableLSTM(128, unique_syllables, training_weights)
 syllable_lstm.train_model(keep_syllables[training],
-                          n_epochs=100,
-                          save_file='test_model.pkl',
-                          loss_file='test_loss.csv',
+                          n_epochs=500,
+                          save_file='weighted_lstm.pkl',
+                          loss_file='weighted.csv',
                           track_time=True)
 c_matrix = syllable_lstm.test_model(keep_syllables[test])
-c_matrix.to_csv('subset_syl_cmatrix.csv')
-plot_confusion(c_matrix, 'subset_syl_cmatrix.png')
+c_matrix.to_csv('weighted_cmatrix.csv')
+SyllablePlots.plot_confusion(c_matrix, 'weighted_cmatrix.png')
